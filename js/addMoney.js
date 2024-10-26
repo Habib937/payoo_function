6b886618a17395474f91192fbfcc6a9dafa92e3f
1987_ Mohammad Habib Ullah <mohammadhabibullah937@gmail.com>
2024-10-26 13:33:24 +0600

    added function with paremeter to call id

--- js/addMoney.js
+++ js/addMoney.js
@@ -2,19 +2,28 @@ document.getElementById('btn-add-money')
     .addEventListener('click', function(event){
         event.preventDefault();
         
-        let inputMoney = document.getElementById('amount-id').value;
-        let inputMoneyNum = parseFloat(inputMoney)
-        let pinNumber = document.getElementById('pin-id').value;
-        console.log(inputMoney,pinNumber)
 
-        if(pinNumber==='1234'){
-            let balance =document.getElementById('main-balance').innerText;
-            let balanceNum=parseFloat(balance)
-            let newBalance = inputMoneyNum + balanceNum;
-            document.getElementById('main-balance').innerText = newBalance;
-        }
-        else{
-            alert('Wrong password')
-        }
+        // let addMoney = getInputFieldValueId();
+        // console.log(addMoney)
+    //     let inputMoney = document.getElementById('amount-id').value;
+    //     let inputMoneyNum = parseFloat(inputMoney)
+    //     let pinNumber = document.getElementById('pin-id').value;
+    //     console.log(inputMoney,pinNumber)
+
+    //     if(pinNumber==='1234'){
+    //         let balance =document.getElementById('main-balance').innerText;
+    //         let balanceNum=parseFloat(balance)
+    //         let newBalance = inputMoneyNum + balanceNum;
+    //         document.getElementById('main-balance').innerText = newBalance;
+    //     }
+    //     else{
+    //         alert('Wrong password')
+    //     }
+
+
+    let addMoney = getInputFieldValueId('amount-id');
+    let pinNumber = getInputFieldValueId('pin-id');
+    console.log(addMoney,pinNumber)
+
 
     })
---
+++ js/utilies.js
@@ -0,0 +1,10 @@
+// function getInputFieldValueId(){
+//     let inputAmount = document.getElementById('amount-id').value;
+//     return inputAmount;
+
+// }
+
+function getInputFieldValueId(id){
+    let inputAmount = document.getElementById(id).value;
+    return inputAmount
+}
