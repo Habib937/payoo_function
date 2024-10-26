c653e17c756ea1383e4248263f12350dccd3157c
1987_ Mohammad Habib Ullah <mohammadhabibullah937@gmail.com>
2024-10-26 21:57:11 +0600

    some dynamic work complete and finally ready the payoo function

--- js/addMoney2.js
+++ js/addMoney2.js
@@ -4,10 +4,21 @@ document.getElementById('btn-add-money')
 
         let addMoney = getInputFieldValueId('amount-id')
         let pinNumber = getInputFieldValueId('pin-id')
+
+        if(isNaN(addMoney)){
+            alert('add money failed')
+            return;
+        }
         if(pinNumber === 1234){
             let balance = getTextFieldValueId('main-balance');
             let newBalance = balance + addMoney;
             document.getElementById('main-balance').innerText = newBalance;
+
+            // add to transition history
+            let p = document.createElement('p')
+            p.innerText = `Added ${addMoney} tk. New Balance ${newBalance}`
+
+            document.getElementById('transaction-container').appendChild(p)
 
 
         }
--- js/cashOut.js
+++ js/cashOut.js
@@ -4,10 +4,35 @@ document.getElementById('btn-cash-out')
 
         let cashOut = getInputFieldValueId('cash-out-amount-id');
         let pinNumber = getInputFieldValueId('cash-out-pin-id');
+
+        if(isNaN(cashOut)){
+            alert('cash out failed');
+            return;
+        }
+
         if(pinNumber === 1234){
             let balance = getTextFieldValueId('main-balance');
+            if(balance<cashOut){
+                alert('you have not enough money')
+                return;
+            }
             let newBalance = balance-cashOut
             document.getElementById('main-balance').innerText = newBalance
+
+            // added to transition history
+
+            let div = document.createElement('div')
+            div.classList.add('bg-yellow-300');
+            div.innerHTML = `
+            <h4 class = "text-2xl font-bold">Cash Out</h4>
+            <p>${cashOut} tk withdraw. New Balance ${newBalance}</p>
+            
+            `
+            document.getElementById('transaction-container').appendChild(div)
+        }
+        else{
+            alert('no money you have')
         }
 
     })
+    
